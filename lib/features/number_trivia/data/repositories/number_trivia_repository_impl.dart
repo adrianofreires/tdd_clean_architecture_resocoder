d534b44c268f8e860d36efc5dba45ef5a0d8fcd1
Adriano Freires <freires.adriano@gmail.com>
2021-03-12 17:30:16 -0300

    aula 7 completo

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,4 +1,5 @@
-import 'package:tdd_clean_architecture_resocoder/core/plataform/network_info.dart';
+import 'package:tdd_clean_architecture_resocoder/core/error/exceptions.dart';
+import 'package:tdd_clean_architecture_resocoder/core/network/network_info.dart';
 import 'package:tdd_clean_architecture_resocoder/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:tdd_clean_architecture_resocoder/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:tdd_clean_architecture_resocoder/features/number_trivia/domain/entities/number_trivia.dart';
@@ -7,21 +8,50 @@ import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
 import 'package:tdd_clean_architecture_resocoder/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
+typedef Future<NumberTrivia> _ConcreteOrRandomChooser();
+
 class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
   final NumberTriviaRemoteDataSource remoteDataSource;
   final NumberTriviaLocalDataSource localDataSource;
   final NetworkInfo networkInfo;
 
-  NumberTriviaRepositoryImpl({@required this.remoteDataSource, @required this.localDataSource, @required this.networkInfo,});
+  NumberTriviaRepositoryImpl({
+    @required this.remoteDataSource,
+    @required this.localDataSource,
+    @required this.networkInfo,
+  });
   @override
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) {
-    // TODO: implement getConcreteNumberTrivia
-    return null;
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(
+      int number) async {
+    return await _getTrivia(() {
+      return remoteDataSource.getConcreteNumberTrivia(number);
+    });
   }
 
   @override
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
-    // TODO: implement getRandomNumberTrivia
-    return null;
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() async {
+    return await _getTrivia(() {
+      return remoteDataSource.getRandomNumberTrivia();
+    });
+  }
+
+  Future<Either<Failure, NumberTrivia>> _getTrivia(
+      _ConcreteOrRandomChooser getConcreteOrRandon) async {
+    if (await networkInfo.isConnected) {
+      try {
+        final remoteTrivia = await getConcreteOrRandon();
+        localDataSource.cacheNumberTrivia(remoteTrivia);
+        return Right(remoteTrivia);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localTrivia = await localDataSource.getLastNumberTrivia();
+        return Right(localTrivia);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
   }
 }
---
+++ test/core/network/network_info_test.dart
@@ -0,0 +1,34 @@
+import 'dart:ffi';
+
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:tdd_clean_architecture_resocoder/core/network/network_info.dart';
+
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+
+void main() {
+  MockDataConnectionChecker mockDataConnectionChecker;
+  NetworkInfoImpl networkInfo;
+
+  setUp(() {
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfo = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+  group('isConnected', () {
+    test(
+      'should forward the call to DataConnectionChecker.hasConnect',
+      () async {
+        // arrange
+        when(mockDataConnectionChecker.hasConnection)
+            .thenAnswer((_) async => true);
+        // act
+        final result = await networkInfo.isConnected;
+        // assert
+        verify(mockDataConnectionChecker.hasConnection);
+        expect(result, true);
+      },
+    );
+  });
+}
