fc7bb03c1140283d2bb95b2b23487ec1d3c14a25
Adriano Freires <freires.adriano@gmail.com>
2021-03-15 10:21:43 -0300

    aula 8 concluída

--- test/core/network/network_info_test.dart
+++ test/core/network/network_info_test.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:data_connection_checker/data_connection_checker.dart';
